c1b355499e3eb404d39f2d362e9b8459b51f3af1
jltrem <joe@lariat.io>
2020-02-23 12:59:22 -0600

    Bump version to 0.2.1
    ## [0.2.1] - 2020-02-23
    
    ### Added
    - added project description
    
    ### Fixed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,11 @@
 # Changelog
 
-## [Unreleased]
+## [0.2.1] - 2020-02-23
 
 ### Added
 - added project description
 
 ### Fixed
-
 
 ## [0.2.0] - 2020-02-23
 
@@ -19,7 +18,8 @@ MiniScaffold test
 ### Added
 - Setup with MiniScaffold
 
-[Unreleased]: https://github.com/jltrem/Fescq/compare/v0.2.0...HEAD
+[Unreleased]: https://github.com/jltrem/Fescq/compare/v0.2.1...HEAD
+[0.2.1]: https://github.com/jltrem/Fescq/compare/v0.2.0...v0.2.1
 [0.2.0]: https://github.com/jltrem/Fescq/compare/v0.1.1...v0.2.0
 [0.1.1]: https://github.com/jltrem/Fescq/compare/v0.1.0...v0.1.1
 [0.1.0]: https://github.com/jltrem/Fescq.git/releases/tag/v0.1.0
--- tests/Fescq.Tests/AssemblyInfo.fs
+++ tests/Fescq.Tests/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Fescq.Tests")>]
 [<assembly: AssemblyProductAttribute("Fescq")>]
-[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.1")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","2020-02-23T00:00:00.0000000-06:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.1")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","fd7eb57558d917d6399e87434a9243e394a50aff")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","542d596082a9240787da7f85d0c590575cae2248")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Fescq.Tests"
     let [<Literal>] AssemblyProduct = "Fescq"
-    let [<Literal>] AssemblyVersion = "0.2.0"
+    let [<Literal>] AssemblyVersion = "0.2.1"
     let [<Literal>] AssemblyMetadata_ReleaseDate = "2020-02-23T00:00:00.0000000-06:00"
-    let [<Literal>] AssemblyFileVersion = "0.2.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.2.0"
+    let [<Literal>] AssemblyFileVersion = "0.2.1"
+    let [<Literal>] AssemblyInformationalVersion = "0.2.1"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "fd7eb57558d917d6399e87434a9243e394a50aff"
+    let [<Literal>] AssemblyMetadata_GitHash = "542d596082a9240787da7f85d0c590575cae2248"
