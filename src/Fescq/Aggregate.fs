7b0f237f72790048aa783b890111244342d2aac5
jltrem <joe@lariat.io>
2020-03-02 23:12:50 -0600

    Finished sanding Aggregate as Projection

--- src/Fescq/Aggregate.fs
+++ src/Fescq/Aggregate.fs
@@ -4,18 +4,35 @@ open Core
 
 type AggregateProjection<'entity> = Projection<EntityState<'entity>, Event>
 
+type private CreateCfg<'entity> = {
+   Key: AggregateKey
+   Events: Event list
+   Projection: AggregateProjection<'entity>
+}
+
 // TODO: consider how to handle hydrating an aggregate when the event validation rules have changed
 // so that an historical event is now considered invalid.  Is this a real concern?
 
-/// Apply all events with the provided ApplyEvent function.
-/// This function must validate domain rules so that construction is successful IFF the state is valid.
+/// Create the aggregate using the provided projection.
+/// This projection function must validate domain rules so that construction is successful IFF the state is valid.
 ///
-/// Note that historical events should always pass this validation, as they were subjected to it
-/// when they were appended (assuming validation rules have not changed); but the future events must fail
-/// (throw an exception) if they put the aggregate in an invalid state.
-let create<'entity> (projection:AggregateProjection<'entity>) (history:Event list) (future:Event list) : Agg<'entity> =
+/// Note that historical events should always pass this validation, as they were subjected to it when they
+/// were appended (assuming validation rules have not changed); but the future events must fail (throw an exception)
+/// if they put the aggregate in an invalid state.
+let private create<'entity> (cfg:CreateCfg<'entity>) : Agg<'entity> =
 
-   let events =
+   cfg.Events
+   |> List.tail
+   |> List.fold cfg.Projection.Update cfg.Projection.Init
+   |> fun state ->
+      { Key = cfg.Key
+        Entity = state.Entity
+        History = cfg.Events }
+
+
+let private makeCreateCfg (projectionFactory: Event -> AggregateProjection<'entity>) (history:Event list) (future:Event list) =
+
+   let events = 
       history @ future
       |> function
          | [] -> failwith "events cannot be empty"
@@ -27,33 +44,26 @@ let create<'entity> (projection:AggregateProjection<'entity>) (history:Event lis
                | [_] -> all
                | _ -> failwith "events must refer to the same aggregate id"
 
-   let key =
-      events
-      |> List.last
-      |> fun x -> x.AggregateKey
+   let key = events |> List.last |> fun x -> x.AggregateKey
 
-   events
-   |> List.tail
-   |> List.fold projection.Update projection.Init
-   |> fun state ->
-      { Key = key
-        Entity = state.Entity
-        History = events }
+   { Key = key
+     Events = events
+     Projection = projectionFactory (List.head events) }
 
 
 let createWithFirstEvent<'entity> (projectionFactory: Event -> AggregateProjection<'entity>) first =
-   let projection = projectionFactory first
-   create projection [] [first]
+   makeCreateCfg projectionFactory [] [first]
+   |> create
 
 
 let createWithNextEvent<'entity> (projectionFactory: Event -> AggregateProjection<'entity>) history next =
-   let projection = projectionFactory (List.head history)
-   create projection history [next]
+   makeCreateCfg projectionFactory history [next]
+   |> create
 
 
 let createFromHistory<'entity> (projectionFactory: Event -> AggregateProjection<'entity>) history =
-   let projection = projectionFactory (List.head history)
-   create projection history []
+   makeCreateCfg projectionFactory history []
+   |> create
 
 
 let projectionFactory<'entity> create update : Event -> AggregateProjection<'entity> =
